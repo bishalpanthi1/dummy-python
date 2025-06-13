e4a74e6ed86ecec5dc66da307ddfa830cdda2d45
Bishal <bishalpanthi1@gmail.com>
2025-06-13 21:27:10 +0545

    update env

--- index.py
+++ index.py
@@ -7,4 +7,6 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the PORT from Render environment or fallback to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
